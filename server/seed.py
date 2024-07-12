5804b3f6425b5ca7711b1af98ef2e6841107f992
brewchetta <chett.tiller@gmail.com>
2024-07-12 10:34:02 -0400

    fixed typo in youtube links, print statements in seed

--- server/seed.py
+++ server/seed.py
@@ -5,10 +5,14 @@ from models import Song
 if __name__ == '__main__':
     with app.app_context():
 
+        Song.query.delete()
+        db.session.commit()
+
         json_seed = open("seed.json")
         data = json.load(json_seed)
 
         for song_item in data['music']:
+            print(f'Creating song {song_item["title"]}...')
             s = Song(title=song_item['title'], artist=song_item['artist'], youtube_link=song_item['youtube_link'])
             db.session.add(s)
 
